12bdf3e6355606152cd248c844270244e2ea4bed
Yasuyuki Goto <66114313+yasuyuki-hand@users.noreply.github.com>
2020-10-12 18:23:48 +0900

    Initial Project (#1)
    Co-authored-by: 後藤康之 <yasuyukigoto@gotouyasuyukinoMacBook-Air.local>

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+
+    validates :name, presence: true, length: { minimum: 2, maximum: 75 }
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    # validates :email, presence: true, length: { minimum: 5, maximum: 200 }
+
+    has_secure_password # password and password confirmation is required
+    validates :password, length: { minimum: 5 }
+end
