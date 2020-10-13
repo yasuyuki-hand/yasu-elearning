66080efaf311b68e93d382731dbe7088963bf223
Yasuyuki Goto <66114313+yasuyuki-hand@users.noreply.github.com>
2020-10-13 18:10:55 +0900

    Login (#2)
    * login
    
    * Added data in gitignore
    
    * fixed spacing
    
    * I fixed spacing
    
    Co-authored-by: 後藤康之 <yasuyukigoto@gotouyasuyukinoMacBook-Air.local>

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,14 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+
+  #/login
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
