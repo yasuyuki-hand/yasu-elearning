439f9f1a2606d51950af44ab8a5064cb198a7753
Yasuyuki Goto <66114313+yasuyuki-hand@users.noreply.github.com>
2020-10-14 15:10:03 +0900

    added logout (#3)
    Co-authored-by: 後藤康之 <yasuyukigoto@gotouyasuyukinoMacBook-Air.local>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :only_loggedin_users, only: [:index, :edit, :update, :destroy, :following, :followers]
+
   def new
     @user = User.new
   end
@@ -18,10 +20,20 @@ class UsersController < ApplicationController
     #show all microposts of this user
     # @microposts = @user.microposts.paginate(page: params[:page], per_page: 10)
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
 
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def only_loggedin_users
+    redirect_to login_url unless logged_in?
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,51 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
+  # get 'user/new' -> /singup or /register
+  resources :users
   root 'static_pages#home'
 
-
-  resources :users
-  
   #HTTP ACTION '/route-name', to: 'controller_name#action'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
-
+  #New create and destroy for sessions
   resources :sessions, only: [:new, :create, :destroy]
 
+  #Create and destroy for Micropost
+  #Create  -> Post button
+  #Destroy -> Delete button to delete post
+  # Update is not here beacuse in 
+  resources :microposts, only: [:create, :destroy]
+  resources :microposts do
+    member do
+      post 'uplike'
+    end
+  end
+  #For follow and unfollow
+  resources :relationships, only: [:create, :destroy]
 
   #/login
   get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
-  
+
+
+  #Follow Stats
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  # localhost:3000 -> default page/root page
+  # localhost:3000/users/1 -> user's page/profile page
+  # localhost:3000/users/1/following -> show user 1's following / who is he following?
+  # localhost:3000/users/1/followers -> shows user 1's followers / how many followers? who is following him?
+
+  # get 'static_pages/home'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
