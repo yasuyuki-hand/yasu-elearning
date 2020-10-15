9fc2f188fa361871ff3114b82ac87669bf5cdb35
Yasuyuki Goto <66114313+yasuyuki-hand@users.noreply.github.com>
2020-10-15 15:16:47 +0900

    Relationship and follow button (#6)
    * Relationship and follow
    
    * fixed follow function
    
    Co-authored-by: 後藤康之 <yasuyukigoto@gotouyasuyukinoMacBook-Air.local>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
+  has_many :active_relationships, foreign_key: "follower_id",
+                                  class_name: "Relationship",
+                                  dependent: :destroy
+
+  has_many :followed_users, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, foreign_key: "followed_id",
+                                     class_name: "Relationship",
+                                     dependent: :destroy
+
+  has_many :followers, through: :passive_relationships, source: :follower
 
   validates :name, presence: true, length: { minimum: 2, maximum: 75 }
 
@@ -8,4 +19,16 @@ class User < ApplicationRecord
 
   has_secure_password # password and password confirmation is required
   validates :password, length: { minimum: 5 }
+
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,5 +21,14 @@ User.create!(name: name,
   email: email,
   password: password,
   password_confirmation: password)
-
 end
+
+
+  users = User.all
+  user = users.first
+  following = users[2..50]
+  followers = users[3..40]
+
+  following.each { |followed| user.follow(followed) }
+  followers.each { |follower| follower.follow(user) }
+
