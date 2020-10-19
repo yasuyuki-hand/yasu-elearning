457dddfd0d96a1c8a8b4cdb412bbc8eb03f7a2d2
後藤康之 <yasuyukigoto@gotouyasuyukinoMacBook-Air.local>
2020-10-19 16:10:47 +0900

    added word list

---
+++ app/controllers/words_controller.rb
@@ -0,0 +1,20 @@
+class WordsController < ApplicationController
+  def index
+    @words = Word.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
